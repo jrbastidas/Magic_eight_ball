354b5ce04f9027eb7b5778d63e15f44ef5b43710
Jose Ricardo Bastidas Florez <103537788+jrbastidas@users.noreply.github.com>
2024-05-10 16:43:37 -0600

    Add files via upload

---
+++ MagicEightBall.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Magic8Ball
+{
+    partial class MagicEightBall
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.QuestionBox = new System.Windows.Forms.TextBox();
+            this.answerLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.SubmitButton.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SubmitButton.ForeColor = System.Drawing.Color.White;
+            this.SubmitButton.Location = new System.Drawing.Point(40, 270);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(124, 69);
+            this.SubmitButton.TabIndex = 0;
+            this.SubmitButton.Text = "Tell me!";
+            this.SubmitButton.UseVisualStyleBackColor = false;
+            this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
+            // QuestionBox
+            // 
+            this.QuestionBox.Font = new System.Drawing.Font("Broadway", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuestionBox.Location = new System.Drawing.Point(12, 174);
+            this.QuestionBox.Multiline = true;
+            this.QuestionBox.Name = "QuestionBox";
+            this.QuestionBox.Size = new System.Drawing.Size(190, 69);
+            this.QuestionBox.TabIndex = 1;
+            this.QuestionBox.Text = "Ask your Question here...";
+            this.QuestionBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // answerLabel
+            // 
+            this.answerLabel.BackColor = System.Drawing.Color.Transparent;
+            this.answerLabel.Font = new System.Drawing.Font("Onyx", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.answerLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.answerLabel.Location = new System.Drawing.Point(433, 229);
+            this.answerLabel.Name = "answerLabel";
+            this.answerLabel.Size = new System.Drawing.Size(178, 33);
+            this.answerLabel.TabIndex = 2;
+            this.answerLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.answerLabel.Click += new System.EventHandler(this.Answer_Click);
+            // 
+            // MagicEightBall
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Magic8Ball.Properties.Resources._8_Ball_Background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1043, 618);
+            this.Controls.Add(this.answerLabel);
+            this.Controls.Add(this.QuestionBox);
+            this.Controls.Add(this.SubmitButton);
+            this.DoubleBuffered = true;
+            this.Name = "MagicEightBall";
+            this.Text = "Magic 8 Ball";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SubmitButton;
+        private System.Windows.Forms.TextBox QuestionBox;
+        private System.Windows.Forms.Label answerLabel;
+    }
+}
+
---
+++ MagicEightBall.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Magic8Ball
+{
+    public partial class MagicEightBall : Form
+    {
+        Random random = new Random();
+        List<String> answers = new List<String>();
+        public MagicEightBall()
+        {
+            InitializeComponent();
+            answers.Add("It is decidely so");
+            answers.Add("Better not tell you now");
+            answers.Add("Concentrate and ask again");
+            answers.Add("Outlook not so good");
+            answers.Add("You may rely on it");
+            answers.Add("Yes definitely");
+            answers.Add("Cannot predict now");
+            answers.Add("Very doubtful");
+        }
+
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            int index = random.Next(answers.Count);
+            answerLabel.Text = answers[index];
+        }
+
+        private void Answer_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
